7d9054895ffb26c865cde37aca53c5079828f0e7
Thevesh <thevesh.theva@gmail.com>
2023-02-13 04:02:02 +0800

    update: sample constants file

---
+++ constants_sample.py
@@ -0,0 +1,7 @@
+# resource links
+GITHUB_DONATIONS = 'https://raw.githubusercontent.com/MoH-Malaysia/data-darah-public/main/donations_state.csv'
+GCS_RETENTION = 'https://storage.googleapis.com/moh-malaysia-public-darah/analysis_retention.parquet'
+
+# tokens
+TELEGRAM_BOT_OWID = 'YOUR BOT TOKEN (visit Botfather on Telegram)'
+TELEGRAM_GROUP_OWID = [TELEGRAM_BOT_OWID, 'Your Group/Channel ID as an integer (use IDBot to get it)']
